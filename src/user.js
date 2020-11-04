98bff5437154e307a2582f9f280a9b897432c8dc
Jacky <newbdez33@gmail.com>
2020-11-04 16:33:57 +0900

    user data

--- src/user.js
+++ src/user.js
@@ -4,14 +4,17 @@ import getUserDataStream from './lib/services/getUserDataStream';
 import SocketClient from './lib/socketClient';
 import renewListenKey from './lib/helpers/renewListenKey';
 import logger from './lib/logger';
+import telegram from './lib/telegram';
+import dotenv from "dotenv";
 
-const APIKEY = process.env.APIKEY || '';
-const APISECET = process.env.APISECET || '';
+dotenv.config();
+const APIKEY = process.env.APIKEY;
+const APISECET = process.env.APISECET;
 
 export default async function createApp() {
   logger.info('start application');
   const listenKey = await getUserDataStream(APIKEY, APISECET);
-
+  telegram('start monitor user data with key:' + listenKey);
   logger.info('key received.', listenKey);
   const socketApi = new SocketClient(`ws/${listenKey}`);
   socketApi.setHandler('executionReport', (params) => logger.info(params));
